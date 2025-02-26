d4f7e8345815dab8674680ffaa53e76bccb3d433
Bahriddinov Muhammadhakim <profiy70@gmail.com>
2025-02-26 14:12:51 +0000

    feat: next.js bot startUp

--- logo.py
+++
@@ -1,8 +0,0 @@
-from colorama import Back
-
-def logo():
-   print(Back.BLACK + "      _______   __      __      __     _______       ")
-   print(Back.BLACK + "     /  ___  \  \ \    /  \    / /    /  ___  \      ")
-   print(Back.BLACK + "    /  /___\  \  \ \  / /\ \  / /    /  /___\  \     ")
-   print(Back.BLACK + "   /  /_____\  \  \ \/ /  \ \/ /    /  /_____\  \    ")
-   print(Back.BLACK + "  /__/       \__\  \__/    \__/    /__/       \__\   ") 
--- main.py
+++
@@ -1,1 +0,0 @@
-from termux_module import *
